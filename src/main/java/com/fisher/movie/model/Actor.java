d3e62d7f8dba0bd65cad0320f5c6aaec5872fd9c
fisher <fisheralexandr@gmail.com>
2022-12-13 19:10:36 +0400

    added actor and manyToMany relation

--- src/main/java/com/fisher/movie/model/Actor.java
+++ src/main/java/com/fisher/movie/model/Actor.java
@@ -3,22 +3,16 @@ package com.fisher.movie.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.Set;
 
 @Entity
 @Data
-public class Movie {
+public class Actor {
     @Id
     @GeneratedValue
     Long id;
-    String title;
-    LocalDate releaseDate;
-    Double rating;
-    Integer duration;
-    @ManyToMany
-    Set<Actor> actors;
+    String name;
+    LocalDate birth;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,12 @@ INSERT INTO movie (id, title) VALUES (1, 'Breaking bad');
 INSERT INTO movie (id, title) VALUES (2, 'Troy');
 INSERT INTO movie (id, title) VALUES (3, 'Inglourious Basterds');
 INSERT INTO movie (id, title) VALUES (4, 'Warrior');
+
+INSERT INTO actor (id, name, birth) VALUES (1, 'Brad Pitt', '1963-12-18');
+INSERT INTO actor (id, name, birth) VALUES (2, 'Tom Hardy', '1977-09-15');
+INSERT INTO actor (id, name, birth) VALUES (3, 'Bryan Cranston', '1956-03-7');
+
+INSERT INTO movie_actors (movie_id, actors_id) VALUES (1, 3);
+INSERT INTO movie_actors (movie_id, actors_id) VALUES (2, 1);
+INSERT INTO movie_actors (movie_id, actors_id) VALUES (3, 1);
+INSERT INTO movie_actors (movie_id, actors_id) VALUES (4, 2);
